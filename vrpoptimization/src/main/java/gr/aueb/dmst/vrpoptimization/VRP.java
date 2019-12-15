43e5285ca842826e5a85727136479e644cd6899c
kiriakivel <kiriaki.vel@gmail.com>
2019-12-15 15:37:51 +0200

    modified VRP file

--- vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/VRP.java
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/VRP.java
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gr.aueb.dmst.vrpoptimization;
+package vrp;
+
 
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
  *
- * @author team7
+ * @author mzaxa
  */
 public class VRP {
 
@@ -21,7 +22,7 @@ public class VRP {
     Node depot;
     int numberOfCustomers;
     int capacity;
-    
+    Solution bestSolutionThroughTabuSearch;
 
     public VRP(int totalCustomers, int cap) {
         numberOfCustomers = totalCustomers;
@@ -31,14 +32,13 @@ public class VRP {
 
     void GenerateNetworkRandomly() {
         CreateAllNodesAndCustomerLists(numberOfCustomers);
-        //CalculateDistanceMatrix();
+        CalculateDistanceMatrix();
     }
 
     public void CreateAllNodesAndCustomerLists(int numberOfCustomers) {
         //Create the list with the customers
         customers = new ArrayList();
-        
-        
+
         for (int i = 0; i < numberOfCustomers; i++) {
             Node cust = new Node();
 
@@ -61,15 +61,13 @@ public class VRP {
             Node cust = customers.get(i);
             allNodes.add(cust);
         }
-        
-        
 
         for (int i = 0; i < allNodes.size(); i++) {
             Node nd = allNodes.get(i);
             nd.ID = i;
         }
-       
     }
+
     public void CalculateDistanceMatrix() {
 
         distanceMatrix = new double[allNodes.size()][allNodes.size()];
@@ -89,5 +87,137 @@ public class VRP {
             }
         }
     }
+
+    void Solve() {
+
+        Solution s = new Solution();
+
+        ApplyNearestNeighborMethod(s);
+        for(int i = 0 ; i < 1000; i++){
+        SolutionDrawer.drawRoutes(allNodes, s, Integer.toString(i));
+        }
+       // TabuSearch(s);
+    }
+
+    private void SetRoutedFlagToFalseForAllCustomers() {
+        for (int i = 0; i < customers.size(); i++) {
+            customers.get(i).isRouted = false;
+        }
+    }
+
+    private void ApplyNearestNeighborMethod(Solution solution) {
+
+        boolean modelIsFeasible = true;
+        ArrayList<Route> routeList = solution.routes;
+
+        SetRoutedFlagToFalseForAllCustomers();
+
+        //Q - How many insertions? A - Equal to the number of customers! Thus for i = 0 -> customers.size()
+        for (int insertions = 0; insertions < customers.size(); /* the insertions will be updated in the for loop */) {
+            //A. Insertion Identification
+            CustomerInsertion bestInsertion = new CustomerInsertion();
+            bestInsertion.cost = Double.MAX_VALUE;
+            Route lastRoute = GetLastRoute(routeList);
+            if (lastRoute != null) {
+                IdentifyBestInsertion_NN(bestInsertion, lastRoute);
+            }
+            //B. Insertion Application
+            //Feasible insertion was identified
+            if (bestInsertion.cost < Double.MAX_VALUE) {
+                ApplyCustomerInsertion(bestInsertion, solution);
+                insertions++;
+            } //C. If no insertion was feasible
+            else {
+                //C1. There is a customer with demand larger than capacity -> Infeasibility
+                if (lastRoute != null && lastRoute.nodes.size() == 2) {
+                    modelIsFeasible = false;
+                    break;
+                } else {
+                    CreateAndPushAnEmptyRouteInTheSolution(solution);
+                }
+            }
+        }
+
+        if (modelIsFeasible == false) {
+            //TODO
+        }
+    }
+
+    private Route GetLastRoute(ArrayList<Route> routeList) {
+        if (routeList.isEmpty()) {
+            return null;
+        } else {
+            return routeList.get(routeList.size() - 1);
+        }
+    }
+
+    private void CreateAndPushAnEmptyRouteInTheSolution(Solution currentSolution) {
+        Route rt = new Route(capacity);
+        rt.nodes.add(depot);
+        rt.nodes.add(depot);
+        currentSolution.routes.add(rt);
+    }
+
+    private void ApplyCustomerInsertion(CustomerInsertion insertion, Solution solution) {
+        Node insertedCustomer = insertion.customer;
+        Route route = insertion.insertionRoute;
+
+        route.nodes.add(route.nodes.size() - 1, insertedCustomer);
+
+        Node beforeInserted = route.nodes.get(route.nodes.size() - 3);
+
+        double costAdded = distanceMatrix[beforeInserted.ID][insertedCustomer.ID] + distanceMatrix[insertedCustomer.ID][depot.ID];
+        double costRemoved = distanceMatrix[beforeInserted.ID][depot.ID];
+
+        route.cost = route.cost + (costAdded - costRemoved);
+        route.load = route.load + insertedCustomer.demand;
+        solution.cost = solution.cost + (costAdded - costRemoved);
+
+        insertedCustomer.isRouted = true;
+    }
+
+    private void IdentifyBestInsertion_NN(CustomerInsertion bestInsertion, Route lastRoute) {
+        for (int j = 0; j < customers.size(); j++) {
+            // The examined node is called candidate
+            Node candidate = customers.get(j);
+            // if this candidate has not been pushed in the solution
+            if (candidate.isRouted == false) {
+                if (lastRoute.load + candidate.demand <= lastRoute.capacity) {
+                    ArrayList<Node> nodeSequence = lastRoute.nodes;
+                    Node lastCustomerInTheRoute = nodeSequence.get(nodeSequence.size() - 2);
+
+                    double trialCost = distanceMatrix[lastCustomerInTheRoute.ID][candidate.ID];
+
+                    if (trialCost < bestInsertion.cost) {
+                        bestInsertion.customer = candidate;
+                        bestInsertion.insertionRoute = lastRoute;
+                        bestInsertion.cost = trialCost;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private double CalculateCostSol(Solution sol)
+    {
+        double totalCost = 0;
+
+        for (int i = 0; i < sol.routes.size(); i++)
+        {
+            Route rt = sol.routes.get(i);
+
+            for (int j = 0; j < rt.nodes.size() - 1; j++) {
+                Node A = rt.nodes.get(j);
+                Node B = rt.nodes.get(j + 1);
+
+                totalCost += distanceMatrix[A.ID][B.ID];
+            }
+        }
+
+        return totalCost;
+
+    }
+
 
 }
