8f861106c72dd81d21224a45b2468b26b9c98b89
kiriakivel <kiriaki.vel@gmail.com>
2019-12-15 15:42:40 +0200

    adding solution and solution drawer classes

---
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/Solution.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vrp;
+
+/**
+ *
+ * @author imakrigiannis
+ */
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mzaxa
+ */
+public class Solution {
+
+        double cost;
+        ArrayList <Route> routes;
+
+        public Solution()
+        {
+            routes = new ArrayList();
+            cost = 0;
+        }
+}
