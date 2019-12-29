feb0e8bcd3a70bf403b75bc51471cc26bb845e5a
ioannavikatou <t8150016@aueb.gr>
2019-12-30 00:01:35 +0200

    Relocation Move added

--- vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/mainClass.java
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/mainClass.java
@@ -14,15 +14,16 @@ import java.util.Random;
  */
 public class mainClass {
 
-    
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Vrp vrp = new Vrp(200, 3000);
-        vrp.GenerateNetworkRandomly();
-        vrp.Solve();
-    }
-    
+     public static void main(String[] args) {
+         // TODO code application logic here
+         Vrp vrp = new Vrp(200, 3000);
+         vrp.GenerateNetworkRandomly();
+         vrp.Solve();
+         vrp.operateLocalSearch(vrp.Solve());
+     }
+
 }
