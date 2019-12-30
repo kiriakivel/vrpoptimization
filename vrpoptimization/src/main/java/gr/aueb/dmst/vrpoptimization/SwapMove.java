a01fc1f613a33384f75d454644c4701528de076a
iasonasma <44037134+iasonasma@users.noreply.github.com>
2019-12-30 16:14:30 +0200

    Create SwapMove.java

---
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/SwapMove.java
@@ -0,0 +1,16 @@
+public class SwapMove
+{
+
+        public int firstRoutePosition;
+        public int firstNodePosition;
+
+        public int secondRoutePosition;
+        public int secondNodePosition;
+
+        public double moveCost;
+
+        public SwapMove()
+        {
+
+        }
+}
