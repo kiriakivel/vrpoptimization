ac4e32382e7d1c4c6548f1f4d67b24f9652c2add
ioannalazaridi <32638674+ioannalaza@users.noreply.github.com>
2020-01-03 20:12:32 +0200

    delete comment

--- vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/Node.java
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/Node.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gr.aueb.dmst.vrpoptimization;
 
-/**
- *
- * @author team7
- */
+
 public class Node {
     
         int x;
--- vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/Solution.java
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/Solution.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gr.aueb.dmst.vrpoptimization;
 
 /**
@@ -12,10 +8,7 @@ package gr.aueb.dmst.vrpoptimization;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author mzaxa
- */
+
 public class Solution {
 
         double cost;
