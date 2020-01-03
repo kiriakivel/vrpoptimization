ef8f2abf8b343c60f768cbb112a9c86fdf2e5d10
ioannalazaridi <32638674+ioannalaza@users.noreply.github.com>
2020-01-03 19:50:33 +0200

    add package

--- vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/TwoOptMove.java
+++ vrpoptimization/src/main/java/gr/aueb/dmst/vrpoptimization/TwoOptMove.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.aueb.dmst.vrpoptimization;
+
 public class TwoOptMove {
     int positionOfFirstRoute;
     int positionOfSecondRoute;
